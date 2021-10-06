de62c74d856690121076fa61e2f1006b2b14d350
Jean Nguyen <jeannguyen.ca@gmail.com>
2021-10-05 22:37:20 -0700

    Basic app router and global styling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,36 @@
-import * as React from 'react';
-import Link from '@mui/material/Link';
+import * as React from 'react'
+import Link from '@mui/material/Link'
+import Container from '@mui/material/Container'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
 
+import Form from './pages/Form'
 
 const App = () => {
+  fetch("http://localhost:5000").then(res => res.json()).then(json => {
+    console.log(json)
+  })
   return (
-    <div className="App">
-      <Link href="#">Form</Link>
-      <Link href="#">List</Link>
-    </div>
+    <Router>
+      <Container>
+        <nav>
+          <Link to="/">Form</Link>
+          <Link to="/data">Data</Link>
+        </nav>
+
+        <Switch>
+          <Route path="/">
+            <Form />
+          </Route>
+          <Route path="/data">
+            <div />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
