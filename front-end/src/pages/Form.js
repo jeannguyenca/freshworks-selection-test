ceb6d0c4e9cbbc9ec29fd133420ab32f3eb96abb
Jean Nguyen <jeannguyen.ca@gmail.com>
2021-10-06 21:55:51 -0700

    Finalize form page

--- front-end/src/pages/Form.js
+++ front-end/src/pages/Form.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
+import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
@@ -9,7 +10,7 @@ import InputAdornment from '@mui/material/InputAdornment'
 import DateAdapter from '@mui/lab/AdapterDateFns'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 
-import LocationAutocompleteInput from '../components/LocationAutocompleteInput'
+import MaterialUIGooglePlaces from '../components/MaterialUIGooglePlaces'
 
 const Form = () => {
   const [datetime, setDatetime] = useState(new Date())
@@ -17,8 +18,9 @@ const Form = () => {
   const [numOfDuck, setNumOfDuck] = useState(0)
   const [foodWeight, setFoodWeight] = useState(0)
 
+  // handle saving changes based on target id
   const handleChange = (e) => {
-    switch (e.target) {
+    switch (e.target.id) {
       case 'numOfDuck':
         setNumOfDuck(parseInt(e.target.value))
         break
@@ -38,54 +40,73 @@ const Form = () => {
     setDatetime(datetime)
   }
 
-  const handleSubmit = () => {
-    console.log(datetime, location, numOfDuck, foodWeight)
+  const handleSubmit = async () => {
+    // if location include description, then assign to description foundLocation otherwise assign location
+    let foundLocation = location && location.description ? location.description : location
+
+    // use ISO string to format datetime for storing purpose
+    let toISOString = new Date(datetime).toISOString()
+
+    const response = await fetch(process.env.REACT_APP_API_URL, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ datetime: toISOString, location: foundLocation, numOfDuck, foodWeight })
+    });
+    return response.json();
   }
 
-  return <Box
-    component="form"
-    sx={{
-      '& .MuiTextField-root': { m: 1, width: '100%' },
-    }}
-    autoComplete="off"
-  >
-    <LocalizationProvider dateAdapter={DateAdapter}>
-      <DateTimePicker
-        label="Please choose a day and time"
-        value={datetime}
-        renderInput={(params) => <TextField {...params} />}
-        onChange={handleChangeDateTime}
-        id="datepicker"
-      />
-    </LocalizationProvider>
-
-    <LocationAutocompleteInput
-      location={location}
-      handleChange={handleChangeLocation}
-      handleSelect={handleChangeLocation} />
-
-    <TextField
-      type="number"
-      helperText="Please enter the number of ducks fed"
-      label="Number of ducks"
-      InputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-      id="numOfDuck"
-      onChange={(e) => { handleChange(e) }}
-    />
-
-    <TextField
-      type="number"
-      helperText="Please enter how much food the ducks are fed"
-      label="Quantity"
-      InputProps={{
-        inputMode: 'numeric', pattern: '[0-9]*', endAdornment: < InputAdornment position="end" > kg</InputAdornment>
+  return <Container maxWidth="sm">
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '100%' },
+        p: 2
       }}
-      id="foodWeight"
-      onChange={(e) => { handleChange(e) }}
-    />
-    <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+      autoComplete="off"
+    >
+      <LocalizationProvider dateAdapter={DateAdapter}>
+        <DateTimePicker
+          label="Please choose a day and time"
+          value={datetime}
+          renderInput={(params) => <TextField {...params} />}
+          onChange={handleChangeDateTime}
+          id="datepicker"
+        />
+      </LocalizationProvider>
 
-  </Box >
+      <MaterialUIGooglePlaces
+        location={location}
+        handleChange={handleChangeLocation}
+        handleSelect={handleChangeLocation}
+      />
+
+      <TextField
+        type="number"
+        helperText="Please enter the number of ducks fed"
+        label="Number of ducks"
+        InputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+        id="numOfDuck"
+        onChange={(e) => { handleChange(e) }}
+      />
+
+      <TextField
+        type="number"
+        helperText="Please enter how much food the ducks are fed"
+        label="Quantity"
+        InputProps={{
+          inputMode: 'numeric', pattern: '[0-9]*', endAdornment: < InputAdornment position="end" > kg</InputAdornment>
+        }}
+        id="foodWeight"
+        onChange={(e) => { handleChange(e) }}
+      />
+      <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+
+    </Box >
+  </Container>
+
 }
 
 export default Form
