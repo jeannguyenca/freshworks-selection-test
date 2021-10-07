8fc58460575d5bd0eedb5e481b74b31aa810f685
Jean Nguyen <jeannguyen.ca@gmail.com>
2021-10-06 22:05:32 -0700

    Change key to row.id

--- front-end/src/pages/Data.js
+++ front-end/src/pages/Data.js
@@ -33,7 +33,7 @@ const Data = () => {
       <TableBody>
         {rows.map((row) => (
           <TableRow
-            key={row.name}
+            key={row.id}
             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
           >
             <TableCell component="th" scope="row">
