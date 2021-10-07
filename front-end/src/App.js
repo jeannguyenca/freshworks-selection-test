f77e5cc1df57091ab6ba1c07335b0cfcc675a68f
Jean Nguyen <jeannguyen.ca@gmail.com>
2021-10-06 21:56:41 -0700

    Link data page and simple styling for navigation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,36 +1,51 @@
 import * as React from 'react'
 import Link from '@mui/material/Link'
+import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  NavLink
 } from "react-router-dom"
 
 import Form from './pages/Form'
+import Data from './pages/Data'
+
+const LinkBehavior = React.forwardRef((props, ref) => (
+  <NavLink ref={ref} {...props} activeStyle={{
+    fontWeight: "bold",
+  }} />
+));
 
 const App = () => {
-  fetch("http://localhost:5000").then(res => res.json()).then(json => {
-    console.log(json)
-  })
   return (
     <Router>
-      <Container>
-        <nav>
-          <Link to="/">Form</Link>
-          <Link to="/data">Data</Link>
-        </nav>
+      <Container disableGutters>
+        <Box component="nav" sx={{
+          width: '100%',
+          height: 30,
+          borderBottom: '1px solid black',
+          typography: 'body1',
+          '& > :not(style) + :not(style)': {
+            ml: 2,
+          },
+          p: 2
+        }}>
+          <Link component={LinkBehavior} exact to="/">Form</Link>
+          <Link component={LinkBehavior} to="/data">Data</Link>
+        </Box>
 
         <Switch>
-          <Route path="/">
-            <Form />
-          </Route>
           <Route path="/data">
-            <div />
+            <Data />
+          </Route>
+          <Route exact path="/">
+            <Form />
           </Route>
         </Switch>
       </Container>
-    </Router>
+    </Router >
   );
 }
 
